2004e1fbf1837db4c7fb331faba7f342d73b2209
Эдуард Баянов <e.bayanov@datareon.ru>
2024-11-04 14:08:56 +0300

    input step 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,12 @@
 //import { Fragment } from 'react';
 import './App.css';
-import React from 'react';
 import Counter from './components/Counter';
 
 function App() {
     return (
         <>
-            {/* комментарий */}
-            {[0, 1, 2].map(function (i) {
-                return (
-                    <React.Fragment key={i}>
-                        Counter {i} defined by{' '}
-                        {`<Counter init='5'
-                            end='${10 + i} />`}{' '}
-                        : <br />
-                        <Counter init={5} end={10 + i} />
-                        <br />
-                        <br />
-                    </React.Fragment>
-                );
-            })}
+     {/* https://github.com/PacktPublishing/ASP.NET-Core-5-and-React-Second-Edition */}
+            <Counter />
         </>
     );
 }
--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,63 +1,29 @@
-import styles from './Counter.module.css';
-import { useState, useEffect } from 'react';
-import { CounterProps } from './Counter';
-//import React from 'react';
+//import styles from './Counter.module.css';
+import React ,{  useState } from 'react';
 
-export default function Counter({ init, end }: CounterProps) {
+export default function Counter() {
     // const init1 = parseInt(init || 0);
-    const [count, SetCount] = useState<number>(init ?? 0);
-    const [start, SetStart] = useState<boolean>(true);
-    // function incr(event: React.MouseEvent<HTMLElement>) {
-    //     SetCount((count) => count + 1);
-    //     console.log(event.type);
-    //     console.log(event.currentTarget);
-    // }
-    //let count: number = 0;
-    useEffect(function () {
-        let timer: any | null = null;
-        if (!start) {
-            //кнопка инкремента не видима,можно инициализировать счетчки
-            timer = setInterval(function () {
-                SetCount((count) => {
-                    const newCount: number = count + 1;
-                    if (end && newCount >= end) SetStart(true);
-                    return newCount;
-                });
-            }, 1000);
-        }
-        return function () {
-            clearInterval(timer);
-        };
-    });
-    function restart() {
-        SetStart(false);
-        SetCount(init!);
+    const [value, setValue] = useState<string>("");
+    
+    function change(event: React.FormEvent<HTMLInputElement>) {
+        setValue(event.currentTarget.value);
     }
+
+    function keyDown(event: React.KeyboardEvent<HTMLInputElement>){
+        console.log(event.key);
+        //allow del tab arrow
+        if(["Backspace", "Delete", "ArrowLeft","ArrowRight","Tab"].includes(event.key)) return;
+    //0to9
+    if(event.key < "0" || event.key > "9") event.preventDefault();
+    
+    }
+
     return (
         <>
-            Initial counter is: {init}
-            <br />
-            {end}
-            <br />
-            <div className={styles.div}>Счетчик : {count}</div>
-            <br />
-            {start ? (
-                <>
-                    <i>Counter in progress</i>&nbsp;
-                    <button onClick={restart}> Start</button>
-                </>
-            ) : (
-                <b>Counter in progress</b>
-            )}
-            {/* {end && count >= end ? (
-                //Первый указывает на то, что вы используете JavaScript-код в JSX, в то время как второй указывает на начало используемого объекта JavaScript.
-                <b style={{ color: 'red', fontSize: '20px' }}>
-                    {' '}
-                    Counter stoped
-                </b>
-            ) : (
-                <i>Counter in progress</i>
-            )} */}
+            <i>Counter in progress</i>&nbsp; 
+            <input type="text" onChange={change} onKeyDown={keyDown}/>
+            <br/>
+            input Value: {value}
         </>
     );
 }
